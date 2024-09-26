019cc254167aa13f0a7691587f2ab89e4f66ce06
hasanNurubuloev <hasan.nurubuloev59@gmail.com>
2024-09-26 15:36:32 +0600

    add Scroll

--- lib/feature/course_detail/presentation/course_detail_screen.dart
+++ lib/feature/course_detail/presentation/course_detail_screen.dart
@@ -11,73 +11,76 @@ class CourseDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int dur = coursesModel.duration;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
       appBar: AppBar(
+        scrolledUnderElevation: 0.0,
         backgroundColor: Colors.white,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              coursesModel.title,
-              style: const TextStyle(
-                fontFamily: FontFamily.alegaya,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Image.asset(
-              'assets/images/image2.jpg',
-              width: 332,
-              height: 296,
-            ),
-            Text(
-              coursesModel.description,
-              textAlign: TextAlign.justify,
-              style: const TextStyle(
-                fontSize: 20,
-                color: Colors.blueGrey,
-                fontFamily: FontFamily.alegaya,
-              ),
-            ),
-            const SizedBox(height: 34),
-            Row(
-              children: [
-                const Text(
-                  'Длительность курса',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 20,
-                  ),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                coursesModel.title,
+                style: const TextStyle(
+                  fontFamily: FontFamily.alegaya,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Spacer(),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0,
-                      vertical: 4,
+              ),
+              Image.asset(
+                'assets/images/image2.jpg',
+                width: 332,
+                height: 296,
+              ),
+              Text(
+                coursesModel.description,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.blueGrey,
+                  fontFamily: FontFamily.alegaya,
+                ),
+              ),
+              const SizedBox(height: 34),
+              Row(
+                children: [
+                  const Text(
+                    'Длительность курса',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
                     ),
-                    child: Text(
-                      '${coursesModel.duration} месяца(-ев)',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
+                  ),
+                  const Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 4,
+                      ),
+                      child: Text(
+                        '${coursesModel.duration} месяца(-ев)',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Align(
+                ],
+              ),
+              const SizedBox(height: 40),
+              Align(
                 alignment: Alignment.bottomCenter,
                 child: Container(
                   margin: const EdgeInsets.only(bottom: 24),
@@ -95,9 +98,9 @@ class CourseDetailScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
